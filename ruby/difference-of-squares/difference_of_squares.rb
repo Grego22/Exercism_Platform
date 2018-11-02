725d0951b0c7b907c30af54d3798b8fcb2ce46d6
Greg Garville Jr <gjgarvil@gmail.com>
2018-11-02 14:26:54 -0400

    completed Squares class, passed 9 + additional user test

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -3,6 +3,14 @@ class Squares
         @number = number
     end
     def square_of_sum
-        @number
+        (1.upto(@number).sum**2)
+    end
+    
+    def sum_of_squares
+        (1..@number).map {|n| n**2}.sum
+    end
+
+    def difference
+        (1.upto(@number).sum**2) - (1..@number).map {|n| n**2}.sum
     end
 end
