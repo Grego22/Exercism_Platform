ac288cb3607bcda1b861de7419bf8750fdc23316
Greg Garville Jr <gjgarvil@gmail.com>
2018-11-02 14:37:30 -0400

    variable refactoring

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,16 +1,17 @@
 class Squares
+    #attr_reader :number
     def initialize(number)
         @number = number
     end
     def square_of_sum
-        (1.upto(@number).sum**2)
+        (1.upto(number).sum**2)
     end
     
     def sum_of_squares
-        (1..@number).map {|n| n**2}.sum
+        (1..number).map {|n| n**2}.sum
     end
 
     def difference
-        (1.upto(@number).sum**2) - (1..@number).map {|n| n**2}.sum
+        (1.upto(number).sum**2) - (1..number).map {|n| n**2}.sum
     end
 end
