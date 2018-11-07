361cfe7c2862cb2b2557702e2b485fd0a1533630
Greg Garville Jr <gjgarvil@gmail.com>
2018-11-07 15:11:08 -0500

    added methods, passed 4 inital rspec tests

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,5 +1,32 @@
 class Luhn
-    def valid
-        
+    def initialize (number)
+        @number = number
     end
+    def self.valid?
+        @number.math
+    end
+
+    def math
+        double = 0 
+        digits.reverse.each_with_index.map {|number,index| 
+        if index % 2 != 0
+          double = number*2 
+            if double * 2 > 9
+                double - 9
+            else
+                double
+            end
+        end}
+    end
+
+    def sum_it
+        math.sum 
+    end
+    
+    private
+
+    def digits
+        @number.to_s.chars.map(&:to_i)
+    end
+
 end
