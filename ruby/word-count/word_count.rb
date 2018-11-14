21e4e59272755626161634478a20c4e904ef8463
Greg Garville Jr <gjgarvil@gmail.com>
2018-11-14 14:50:18 -0500

    added private method, passed all tests

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,4 +1,15 @@
-class WordCount
+class Phrase
+    attr_accessor :words
+
+    def initialize(words)
+        @words = words 
+    end
+    def word_count
+        downcaser.each_with_object(Hash.new(0)) { |word, acc| acc[word] += 1 }
+    end
     
-    
+    private
+    def downcaser
+        words.downcase.scan(/\b[\w']+\b/)
+    end
 end
