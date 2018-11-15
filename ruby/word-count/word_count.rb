4ce2f3f1c29ffad037c0921a8bee765883b1ebbe
Greg Garville Jr <gjgarvil@gmail.com>
2018-11-14 22:12:06 -0500

    fixed indentation on phrase class

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,15 +1,15 @@
 class Phrase
-    attr_accessor :words
+  attr_accessor :words
 
-    def initialize(words)
-        @words = words 
-    end
-    def word_count
-        downcaser.each_with_object(Hash.new(0)) { |word, acc| acc[word] += 1 }
-    end
+  def initialize(words)
+    @words = words 
+  end
+  def word_count
+    downcaser.each_with_object(Hash.new(0)) { |word, acc| acc[word] += 1 }
+   end
     
-    private
-    def downcaser
-        words.downcase.scan(/\b[\w']+\b/)
-    end
+  private
+  def downcaser
+    words.downcase.scan(/\b[\w']+\b/)
+  end
 end
