934d5491b1049384957e672f83cd9ee4aab4c678
Greg Garville <gjgarvil@gmail.com>
2019-02-28 18:01:06 -0500

    jumped to python branch and worked on a few exercisms

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,23 @@
 class Allergies(object):
 
+    item_dictionary = {
+        "eggs" : 1,
+        "peanuts" : 2,
+        "shellfish" : 4,
+        "strawberries" : 8,
+        "tomatoes" : 16,
+        "chocolate" : 32,
+        "pollen" : 64,
+        "cats" : 128
+    }
+
     def __init__(self, score):
-        pass
+        self._lst = score
+        
 
     def is_allergic_to(self, item):
         pass
 
     @property
     def lst(self):
-        pass
+        return self._lst
